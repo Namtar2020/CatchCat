328ae864d4c1d0239e10928a93358e35f98573e4
Ali Arif KURU <aakuru@icloud.com>
2020-03-22 21:34:09 +0300

    Initial Commit

---
+++ CatchYeliz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchYeliz
+//
+//  Created by Ali Arif Kuru on 22.03.20.
+//  Copyright © 2020 Ali Arif Kuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
