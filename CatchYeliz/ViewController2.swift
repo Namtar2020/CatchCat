3f03c5b0f2ff09df00d873b152e298afc404740b
Ali Arif KURU <aliarifkuru@gmail.com>
2020-03-24 13:13:13 +0300

    first commit

---
+++ CatchYeliz/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  CatchYeliz
+//
+//  Created by Ali Arif Kuru on 22.03.20.
+//  Copyright © 2020 Ali Arif Kuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
